1a4dabd38246c4c5576c13a72ee1c880358dc2bf
Jiang <langteeth@163.com>
2021-01-30 10:44:25 +0800

    update报名列表

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/attendance">2020训练出勤表</router-link>
-    </div>
+
     <router-view/>
   </div>
 </template>
--- src/views/attendance.vue
+++ src/views/attendance.vue
@@ -1,65 +1,96 @@
 <template>
     <div>
-        <el-table :data="tableData" border>
-            <el-table-column v-for="(val,key) in tableLabel" :key="key" :prop="key" :label="val" width="120">
-            </el-table-column>
-            <el-table-column width="120" v-for="(val,key) in trainningDates" :key="key"  :label="val" >
-            </el-table-column>
-        </el-table>
-        <div v-for="(item,index) in signList" :key="item.id">{{index+1}}.{{item.date}}
-            <ul>
-                <li v-for="(subItem,subIndex) in item.nameList" :key="subIndex"> {{subIndex + 1}}.{{subItem}}</li>
-            </ul>
-        </div>
-
+        <el-form>
+            <el-formItem label="报名表">
+                <el-input type="textarea" class="sign" v-model="signList">
+                </el-input>
+            </el-formItem>
+        </el-form>
+        <el-row>
+            <el-col :span="1">
+                <el-button type="primary" @click="convert">转换</el-button>
+            </el-col>
+            <el-col :span="1" style="margin-right: 10px;">
+                <el-input v-model="newperson"></el-input>
+            </el-col>
+            <el-col :span="2">
+                <el-button type="primary" @click="add">添加</el-button>
+            </el-col>
+            <el-col :span="1" style="margin-right: 10px;">
+                <el-input v-model="multy"></el-input>
+            </el-col>
+            <el-col :span="2">
+                <el-button type="primary" @click="addMore">添加多个</el-button>
+            </el-col>
+            <el-col :span="2">
+                <el-button type="danger" @click="clear">清空</el-button>
+            </el-col>
+        </el-row>
+        <!--        <el-row>-->
+        <!--            <el-col :span="4">-->
+        <!--                <ul>-->
+        <!--                    <li v-for="(item, index) in signData" :key="index"> {{index + 1}}.{{item}}</li>-->
+        <!--                </ul>-->
+        <!--            </el-col>-->
+        <!--        </el-row>-->
+        <!--        <div>-->
+        <!--            {{$data}}-->
+        <!--        </div>-->
     </div>
 </template>
 
 <script>
     export default {
-        props: {
-            isShowOperate: {
-                type:Boolean,
-                default:true
-            }
-        },
         name: "attendance",
         data() {
             return {
-                tableData: [],
-                tableLabel: {
-                    name1:'姓名',
-                    gender1:'性别',
-
-
-                },
-                allDates:[],
-                trainningDates:[],
-                signList:[],
-
+                signList: ``,
+                signData: [],
+                newperson: '',
+                multy:0
 
             }
         },
-        computed: {
-
-        },
+        computed: {},
         methods: {
-            getList() {
-                this.$axios({
-                    method:'get',
-                    url:'http://localhost:3000/signList'
-                }).then(data => {
-                    this.signList = data.data
-                })
+            convert() {
+                this.signData = this.signList.split('\n').map(_ => _.split('.')[1].trim()).filter(_ => _ && _.trim())
+                //  this.signData = this.signList.split('\n').map(_=>_.split('.')[1].trim())
+                this.signList = this.signData.map((item, index) => (index + 1) + '.' + item + '\n').join('')
+            },
+            add() {
+                if (this.newperson) {
+                    this.signData.push(this.newperson)
+                    this.signList = this.signData.map((item, index) => (index + 1) + '.' + item + '\n').join('')
+                } else {
+                    this.$message.info('请添加姓名')
+                }
+
+            },
+            addMore() {
+                for (let i = 0;i<this.multy;i++) {
+                    this.signData.push('')
+                }
+                this.signList = this.signData.map((item, index) => (index + 1) + '.' + item + '\n').join('')
+            },
+            clear() {
+                this.signList = ''
+                this.signData = []
             }
         },
         created() {
 
-            this.getList()
         }
     }
 </script>
 
 <style scoped>
+    li {
+        list-style: none;
+    }
+
+    .sign >>> .el-textarea__inner {
+        height: 400px;
+    }
 
 </style>
